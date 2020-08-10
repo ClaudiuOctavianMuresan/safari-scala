fa52faa13390033b880d9e4ca6b0ceca05ff598b
Simon Roberts <simon@dancingcloudservices.com>
2020-08-10 11:24:22 -0600

    Break One

--- src/main/scala/hello/Hello.scala
+++ src/main/scala/hello/Hello.scala
@@ -1,7 +1,15 @@
 package hello
 
 object Hello {
-  def main(args: Array[String]): Unit = {
-    println("Hello world")
+  def main(args:Array[String]): Unit = {
+//    Console.println("Hello World!")
+    println("Hello World!")
+    println("Still running!")
+
+//    val x: Int = 99 // val is like final in Java and const in JavaScript/C#
+    val x = 99
+//    x = x + 1
+    val xPlusOne = x + 1
+    println(s"The value of x is $x and with an extra hundred that makes ${x + 100}")
   }
 }
